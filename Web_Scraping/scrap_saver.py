b58d391b841c546c96e5e9b4cb7b658937ad7c93
Paplex <pierre-adrien.plessix@mines-paristech.fr>
2020-03-30 14:07:35 +0200

    modularity of the iterator

--- Web_Scraping/scrap_saver.py
+++ Web_Scraping/scrap_saver.py
@@ -1,4 +1,4 @@
-from table_scraper import table_scraper
+from table_scraper import table_scraper_iterator, table_scraper
 from graph_scraper import graph_scraper
 import datetime
 import csv
@@ -16,62 +16,43 @@ def filename_constructor(directory,info,active, price_type):
     return filename
 
 
-def data_initializer(directory, price_type ):
+def data_initializer(directory, price_type):
     """Sparse as much data as possible supposing that there is no one existing before
     Create the data base architecture
     """
-    if price_type == "forward":
-        for info, active, table in table_scraper():
-            filename = filename_constructor(directory, info, active, price_type)
-            table = table.sort_values('Trading Day')
-            table.to_csv(filename, index = False)
-
-    if price_type == "spot":
-        for unit, active, table in graph_scraper():
-            filename = filename_constructor(directory, unit, active, price_type)
-            table = table.sort_values('Trading Day')
-            table.to_csv(filename, index = False)
+    functions= {"forward": table_scraper_iterator, "spot" :graph_scraper }
+    for info, active, table in function[price_type]():
+        filename = filename_constructor(directory, info, active, price_type)
+        table = table.sort_values('Trading Day')
+        table.to_csv(filename, index = False)
 
 
 
 
-def data_updater(directory, price_type ):
+
+
+def data_updater(directory, price_type, specific_type = False ):
     """Read the daily data and add to the existing DB only the missing one 
     """
-    if price_type == "forward":
-        for info, active, table in table_scraper(): 
-            filename = filename_constructor(directory, info, active,price_type)
-            existing_data = pd.read_csv(filename)
-            existing_data = existing_data.sort_values('Trading Day')
-            existing_data['Trading Day'] = pd.to_datetime(existing_data['Trading Day'],format = '%Y-%m-%d')
-            table['Trading Day'] = pd.to_datetime(table['Trading Day'],format = '%Y-%m-%d')
-            last_date  = max(existing_data['Trading Day'])
 
-            table = table[ table['Trading Day'] > last_date ]
-            table = table.sort_values('Trading Day')
+    functions= {"forward": table_scraper_iterator, "spot" :graph_scraper }
+    for info, active, table in functions[price_type](specific_type): 
+        filename = filename_constructor(directory, info, active,price_type)
+        existing_data = pd.read_csv(filename)
+        existing_data = existing_data.sort_values('Trading Day')
+        existing_data['Trading Day'] = pd.to_datetime(existing_data['Trading Day'],format = '%Y-%m-%d')
+        table['Trading Day'] = pd.to_datetime(table['Trading Day'],format = '%Y-%m-%d')
+        last_date  = max(existing_data['Trading Day'])
 
-            existing_data = existing_data.append(table, sort = False)
-            existing_data.to_csv(filename, index = False) 
-    elif price_type == 'spot':
-        
-        for unit, active, table in graph_scraper():
+        table = table[ table['Trading Day'] > last_date ]
+        table = table.sort_values('Trading Day')
 
-            filename = filename_constructor(directory, unit, active,price_type)
-            existing_data = pd.read_csv(filename)
-            existing_data = existing_data.sort_values('Trading Day')
-            existing_data['Trading Day'] = pd.to_datetime(existing_data['Trading Day'],format = '%Y-%m-%d')
-            table['Trading Day'] = pd.to_datetime(table['Trading Day'],format = '%Y-%m-%d')
-            last_date  = max(existing_data['Trading Day'])
-            table = table[ table['Trading Day'] > last_date ]
-            table = table.sort_values('Trading Day')
-
-            existing_data = existing_data.append(table, sort = False)
-            existing_data.to_csv(filename, index = False)
-
+        existing_data = existing_data.append(table, sort = False)
+        existing_data.to_csv(filename, index = False) 
 
 def main():
-    data_initializer('./Web_Scraping','spot')
-    #data_updater('./Web_Scraping','forward')
+    #data_initializer('./Web_Scraping','spot')
+    data_updater('./Web_Scraping','spot')
     #data_updater('./Web_Scraping','spot')
 
 if __name__ == '__main__':
