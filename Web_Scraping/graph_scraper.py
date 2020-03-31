16be080151290a4e40fd513fea87547d5677fccb
Paplex <pierre-adrien.plessix@mines-paristech.fr>
2020-03-31 14:05:02 +0200

    graph_scraper stable pour la dernière date

--- Web_Scraping/graph_scraper.py
+++ Web_Scraping/graph_scraper.py
@@ -33,16 +33,12 @@ def data_list_append(prices, price, dates, date,prediction_types, prediction_typ
     Handle the WE doublon
     """
     if '/' in date:
-        
-        if len(prediction_types) and prediction_types[-1] == 'WE':
-
-            prices.append(price)
-            prediction_types.append(prediction_type)
-            dates.append(date[:-5]+date[-2:])    
-        else :     
-            prices.append(price)
-            prediction_types.append(prediction_type)
-            dates.append(date[:-3])
+        prices.append(price)
+        prediction_types.append(prediction_type)
+        dates.append(date[:-3])
+        prices.append(price)
+        prediction_types.append(prediction_type)
+        dates.append(date[:-5]+date[-2:])  
     else : 
         prices.append(price)
         prediction_types.append(prediction_type)
@@ -96,13 +92,14 @@ class Browser :
                 # find the graph
 
                 chart_container = self.bloc.find_elements_by_class_name('highcharts-graph')[0]
-
                 string_data = chart_container.value_of_css_property("d")
                 chart_container = self.bloc.find_elements_by_class_name('highcharts-graph')[0]
                 webdriver.ActionChains(self.driver).move_to_element(chart_container).perform()
                 pos_data = pos_data_extract(string_data)
                 print(pos_data)
                 ####
+                init_pos_x = pos_data[0][0]
+                init_pos_y = pos_data[0][1]
                 min_x = min(pos_data, key= lambda x : x[0])[0]
                 max_x = max(pos_data, key= lambda x : x[0])[0]
                 min_y = min(pos_data, key= lambda x : x[1])[1]
@@ -113,38 +110,38 @@ class Browser :
                 # Determine coordinate of the middle of the box
                 origin_x = 439  
                 origin_y = 917
-                pos_rel_middle_x = width/2 + origin_x +min_x
-                pos_rel_middle_y = height/2 + origin_y + min_y
+                pos_abs_middle_x = width/2 + origin_x + min_x
+                pos_abs_middle_y = height/2 + origin_y + min_y
+              
+                active_pos_x = pos_abs_middle_x
+                active_pos_y = pos_abs_middle_y
 
-                webdriver.ActionChains(self.driver).move_to_element(chart_container).perform()
-                
-                
-                active_pos_x = pos_rel_middle_x
-                active_pos_y = pos_rel_middle_y
+                offset_x = init_pos_x + origin_x - active_pos_x
+                offset_y = init_pos_y + origin_y - active_pos_y
+                webdriver.ActionChains(self.driver).move_by_offset(offset_x,offset_y).perform()
+                active_pos_x = init_pos_x
+                active_pos_y = init_pos_y
+
                 dates, prediction_types, prices  = [],[],[]
-                for new_pos_x,new_pos_y in pos_data:
-                    print( new_pos_x + origin_x)
-                    print(new_pos_y + origin_y)
-                    #Calculate the offset of the mousz move to touch the last point 
-                    offset_x = new_pos_x + origin_x - active_pos_x
-                    print(offset_x)
-                    offset_y = new_pos_y + origin_y - active_pos_y
-                    offset_y = 0
-                    webdriver.ActionChains(self.driver).move_by_offset(offset_x,offset_y).perform()
-
-                    active_pos_x = new_pos_x + origin_x
-                    active_pos_y = new_pos_y + origin_y       
+                last_date = None
+                step = 10
+                while active_pos_x <= max_x   :
                     
                     label = self.driver.find_element_by_class_name("highcharts-label")
                     texte = label.find_element_by_tag_name("text")
                     texte = texte.find_elements_by_tag_name("tspan")
                     print(texte[0].text.split(' '))
                     prediction_type, date = texte[0].text.split(' ')
-                    info_price = texte[1].text.split(' ')
-                    unit = info_price[1]
-                    price = float(info_price[0])
-
-                    data_list_append(prices, price, dates, date, prediction_types, prediction_type)
+                    if date == last_date :
+                        pass
+                    else :     
+                        info_price = texte[1].text.split(' ')
+                        unit = info_price[1]
+                        price = float(info_price[0])
+                        last_date = date
+                        data_list_append(prices, price, dates, date, prediction_types, prediction_type)
+                    active_pos_x = active_pos_x + step
+                    webdriver.ActionChains(self.driver).move_by_offset(step,0).perform()
                 table = pd.DataFrame({'Trading Day': dates,'Prediction Type': prediction_types,'Price' : prices}) 
                 yield unit, active, table
             else:
