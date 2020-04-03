9058e1f303a6d59aa3feac5ceb94a4a67c78e60a
Paplex <pierre-adrien.plessix@mines-paristech.fr>
2020-04-03 21:05:48 +0200

    ptite correction

--- storage_optimisation/data_structurator.py
+++ storage_optimisation/data_structurator.py
@@ -7,7 +7,7 @@ def structurator( filename = './storage_optimisation/spot_history_HH.xls' ):
     conversion_btu = 0.294
     data = pd.read_excel(filename, sheet_name = 'Data 1', header = 2)
     data.columns = ['Day','Price']
-    data['Price'] = data['Price']*0.294
+    data['Price'] = data['Price']/conversion_btu
     data.to_csv(filename[:-4] + '.csv', index = False)
 
 if __name__ == '__main__':
