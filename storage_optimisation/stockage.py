69122e847d6dec2384c15300fd3d3feaf5e9ccf0
Pierre Adrien PLESSIX <pierre-adrien.plessix@mines-paristech.fr>
2020-06-02 14:15:13 +0200

    before merge

--- storage_optimisation/stockage.py
+++ storage_optimisation/stockage.py
@@ -1,49 +1,77 @@
-import matplotlib.pyplot as plt 
-import numpy as np
-from matrices import Matrices
+"""
+Module that models a  Gaz - Stockage  with different
+paramaters on injection, racking, capacity and 
+"""
 import datetime
-import pandas as pd
 import sys
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from matrices import Matrices
+
+
 class Stockage():
-    def __init__(self, Vmax, Vinit, data, evolution):
+    """
+    Class that implements the storage properties
+    """
+    def __init__(self, Vmax, Vinit, data, evolution, comp_tunnel = True):
+        # Base parameters
         self.Vmax = Vmax
         self.Vinit = Vinit
+        
+        # Time-Series parameters
         self.evolution = evolution
         self.data  = data
+        self.dates = data['Day']
         self.index_i = data.index[0]
         self.index_f = data.index[-1]
-        self.dates = data['Day']
+
+        # Very useful Parameters
         self.N = len(self.dates)
         self.m = Matrices(self.N)
         self.type = "generique"
 
-        # Capacités de soutirage
+        # Capacités de soutirage - ici Générique
         self.D_nom_sout = 44
         Y_0 = [0,0.4]
         Y_1 = [0.17, 0.65]
         Y_2 = [1,1]
         self.sout_list = [Y_0,Y_1,Y_2]
-        # Capacités d'injection
+
+        # Capacités d'injection - ici Générique
         self.D_nom_inj  = 85 
         X_0 = [0,1]
         X_1 = [0.6, 1]
         X_2 = [1,0.43]
         self.inj_list = [X_0,X_1,X_2]
 
+        # Contraintes de remplissage - ici Générique 
         self.months_con = {'04' : [0,0.4],'06' :[0.2,0.65],
                                     '08': [0.5,0.9],'09' : [0,0.95],
                                     '11':[0.85,1]}
 
-        self.tunnel()
+        # calcul du tunnel
+        if comp_tunnel : 
+            self.tunnel()
+    
     def volume_vect (self):
+        """ Function that computes the time-serie of the volume
+        the function uses a triangular matrix and the initial volume to compute the volume 
+        """
         v_vect = self.Vmax*np.dot(self.m.triang_inf,self.evolution) + self.Vinit*np.ones(self.N)
         return v_vect
 
     v = property(volume_vect)
     
     def volume_calc(self) :
+        """
+        Compute the final volume in the storage 
+        Return : 
+            - float
+        """
         return self.volume_vect()[-1]
     
     volume_end = property (volume_calc)
@@ -51,24 +79,50 @@ class Stockage():
     def plot_volume(self):
         plt.plot(self.dates,self.volume_vect())
     
-    def sout_correction(self, v ):
+    def sout_correction(self, v : np.array ):
+        """Function that computes the effective maximale racking of the storage 
+        parameters :
+            - Effective gas volume in the storage
+        Return : 
+            - Maximal percentage of the Vmax rackable 
+        """
         stock_level = v/self.Vmax
         for i in range(1,len(self.sout_list)): 
             if stock_level <= self.sout_list[i][0]: # A rendre +modualir
                 return 1/self.D_nom_sout*(self.sout_list[i-1][1] + (self.sout_list[i][1] - self.sout_list[i-1][1])/(self.sout_list[i][0]-self.sout_list[i-1][0])*(stock_level-self.sout_list[i-1][0]))
         return 0
-    def sout_corrige(self,X):
+    def sout_corrige(self,X : np.array):
+        """
+        Function that for an evolution time serie determines the maximal racking
+        Parameters : 
+            - Effective evolution in the storage 
+        Return : 
+            - np.array() : Maximal percentage of the Vmax rackable in the storage each day 
+        """
         V = self.Vmax*np.dot(self.m.triang_inf,X) + self.Vinit*np.ones(self.N)
         sout_correction  = np.vectorize(self.sout_correction)
         return -1*sout_correction(V) 
     
     def inj_correction(self,v):
+        """Function that computes the effective maximal injection of the storage 
+        parameters :
+            - Effective gas volume in the storage
+        Return : 
+            - Maximal percentage of the Vmax injectable 
+        """
         stock_level = v/self.Vmax
         for i in range(1,len(self.inj_list)):
             if stock_level <= self.inj_list[i][0]:# A Rendre + modulaire
                 return 1/self.D_nom_inj*(self.inj_list[i-1][1] + (self.inj_list[i][1]-self.inj_list[i-1][1])/(self.inj_list[i][0]-self.inj_list[i-1][0])*(stock_level-self.inj_list[i-1][0]))
         return 0
-    def inj_corrige(self,X):  
+    def inj_corrige(self,X):
+        """
+        Function that for an evolution time serie determines the maximal injection each day
+        Parameters : 
+            - Effective evolution in the storage 
+        Return : 
+            - np.array() : Maximal percentage of the Vmax injectable in the storage each day 
+        """  
         V = self.Vmax*np.dot(self.m.triang_inf,X) + self.Vinit*np.ones(self.N)
         inj_correction = np.vectorize(self.inj_correction)
         return inj_correction(V)
@@ -117,6 +171,8 @@ class Stockage():
     threshold_con = property(threshold)
     
     def min_v(self):
+        """function that creates 
+        """
         self.data.loc[:,'minV'] = 0
         for i in self.threshold_con.keys():
             self.data.loc[i,'minV'] = self.threshold_con[i][0]      
@@ -136,7 +192,7 @@ class Stockage():
     vect_max = property (max_v)
     
     def tunnel_max(self):
-        L=[] # Construire directement à la bonne taile 
+        L=np.zeros((self.N,len(self.threshold_con.keys()))) # Construire directement à la bonne taile 
         def chapiteau_max(i,dirac):
             serie = np.zeros(self.N)
             i_max = self.N-1
@@ -148,14 +204,13 @@ class Stockage():
                 if i - pas >= i_min : 
                     serie[i-pas] = serie[i-pas+1] + self.sout_correction(self.Vmax*serie[i-pas+1])
             return serie        
-        for i in self.threshold_con.keys():
-            L .append(self.Vmax*chapiteau_max(i -self.index_i,self.threshold_con[i][1]))
-        # plt.plot(self.dates,np.array(L).transpose())
-        # plt.plot(self.dates,np.zeros(self.N))
-        # plt.title('tete')
-        return np.array(L).min(axis =0  )         
+        for index, j  in enumerate(self.threshold_con.keys()):
+            L [index] = (self.Vmax*chapiteau_max(j -self.index_i,self.threshold_con[j][1]))
+        L = np.vstack((L,self.Vmax*np.ones(self.N)))
+        return np.array(L).min(axis =0) 
+
     def tunnel_min(self):
-        L=[]
+        L=np.zeros((self.N,len(self.threshold_con.keys())))
         def chapiteau_min(i,dirac):
             serie = np.zeros(self.N)
             i_max = self.N-1
@@ -164,25 +219,19 @@ class Stockage():
             for pas in range(1,self.N):
                 if i + pas <= i_max:
                     serie[i+pas] = serie[i+pas-1] - self.sout_correction(self.Vmax*serie[i+pas-1])
-                if i - pas >=0 : 
+                if i - pas >= i_min : 
                     serie[i-pas] = serie[i-pas+1] - self.inj_correction(self.Vmax*serie[i-pas+1])
             return serie        
-        for i in self.threshold_con.keys():
-            L .append(self.Vmax*chapiteau_min(i -self.index_i,self.threshold_con[i][0]))
+        for index, j in enumerate(self.threshold_con.keys()):
+            L[index] = (self.Vmax*chapiteau_min(j -self.index_i,self.threshold_con[j][0]))
         L = np.array(L)
         L = np.vstack((L,np.zeros(self.N)))
-        # plt.plot(self.dates,np.array(L).transpose())
-        # plt.plot(self.dates,np.zeros(self.N))
-        # plt.title('tete')
-        # plt.show()
         return np.array(L).max(axis =0 )
+
     def tunnel(self):
-        plt.plot(self.dates,self.tunnel_max())
-        plt.plot(self.dates,self.tunnel_min())
+        # create the tunnel 
         self.lim_min =   self.tunnel_min()
         self.lim_max = self.tunnel_max()
-
-                
 
     def plot_threshold(self):
         plt.plot(self.dates,self.vect_min)
@@ -192,6 +241,7 @@ class Stockage():
         plt.plot(self.dates,self.lim_min)
         plt.plot(self.dates,self.lim_max)
     def plot_injection(self):
+
         var_sup = self.inj_corrige(self.evolution)
         var_inf = self.sout_corrige((self.evolution))
         plt.plot(self.dates,self.evolution, label = 'evol')
@@ -206,6 +256,9 @@ class Stockage():
 
 
 class Sediane_Nord_20 (Stockage):
+    """
+    Property of a Sediane_Nord_20 storage
+    """
     def __init__(self,Vmax,Vinit, dates, evolution):
         Stockage.__init__(self, Vmax, Vinit,dates, evolution)
         self.type = 'Sediane_Nord_20'
@@ -228,6 +281,9 @@ class Sediane_Nord_20 (Stockage):
                             '11':[0.85,1]}
 
 class Saline_20 (Stockage):
+    """
+    Property of a Saline_20 storage
+    """
     def __init__(self,Vmax,Vinit, dates, evolution):
         Stockage.__init__(self, Vmax, Vinit,dates, evolution)
         self.type = 'Saline_20'
@@ -252,6 +308,10 @@ class Saline_20 (Stockage):
 
 
 class Serene_Atlantique_20 ( Stockage) :
+    """
+    Property of a Serene_Atlantique_20 storage
+    """
+
     def __init__(self,Vmax,Vinit, dates, evolution):
         Stockage.__init__(self, Vmax, Vinit,dates, evolution)
         self.type = 'Serene Atlantique 20 '
@@ -277,6 +337,9 @@ class Serene_Atlantique_20 ( Stockage) :
 
 
 class Serene_Nord_20 ( Stockage) :
+"""
+Property of a Serene_Nord_20 storage
+"""
     def __init__(self,Vmax,Vinit, dates, evolution):
         Stockage.__init__(self, Vmax, Vinit,dates, evolution)
         self.type = 'Serene Nord 20 '
@@ -301,6 +364,9 @@ class Serene_Nord_20 ( Stockage) :
         self.months_con = {'04':[0,0.4], '08':[0,0.9], '09':[0,0.95], '11':[0.9,1]}
 
 def main():
+    """
+    Main functino that show some state of the storage
+    """
     path_spot = Path(__file__).parent.parent / 'Data' / 'spot_history_HH.csv'
 
     data =  pd.read_csv(path_spot)
@@ -319,6 +385,9 @@ def main():
 
 
 def main2():
+    """
+    Main function that shows the tunnel
+    """
     path_spot = Path(__file__).parent.parent / 'Data' / 'spot_history_HH.csv'
 
     data =  pd.read_csv(path_spot)
@@ -334,7 +403,4 @@ def main2():
 
 if __name__ == "__main__":
     sys.exit(main2())
-
-        
-
 
